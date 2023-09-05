976529a8b313c14984923f2f67d2a6dd1b633a32
Jinwoo Lim <1coolbobby159@gmail.com>
2023-09-05 16:30:07 -0400

    added correct route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/reactportfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
