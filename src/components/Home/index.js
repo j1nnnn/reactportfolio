ba095506a54a3f1fd6c81c323dc3961659b694d5
Jinwoo Lim <79156255+j1nnnn@users.noreply.github.com>
2023-09-06 12:37:39 -0400

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-s.png';
+import LogoTitle from '../../assets/images/logo-j.png';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
 import './index.scss';
